95a87f49755a96aba1e1a61049f22134510fa90d
Elavarasu <elavarasu.subramaniam@gmail.com>
2022-06-03 21:55:55 +0530

    JavaFile

---
+++ JavaDeveloper.java
@@ -0,0 +1,18 @@
+package Abstraction;
+
+public class JavaDeveloper extends Worker{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 JavaDeveloper j =  new  JavaDeveloper();
+         j.getHike();
+         
+	}
+	public void work()
+	{
+		System.out.println("Devloped a project using java");
+		//we include the method defination for abstract methods//
+		// otherwise we cannot create object//
+	}
+
+}
