7c3b180abd4dedbf5b236f9104a3cf0b48ac4f0a
Elavarasu <elavarasu.subramaniam@gmail.com>
2022-07-03 22:29:19 +0530

    JavaFile

---
+++ Stranger1.java
@@ -0,0 +1,14 @@
+package Inheritance2;
+import Inheritance.Parent;
+
+public class Stranger1 {
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+	 Parent p = new Parent();
+	//We cannot Access the protected and default values in another package//
+
+	}
+
+}
