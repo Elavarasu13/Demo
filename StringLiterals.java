3dffe8874cf1a585de5368613e5eedd1f15581a2
Elavarasu <elavarasu.subramaniam@gmail.com>
2023-08-24 22:21:41 +0530

    JavaFile

---
+++ StringLiterals.java
@@ -0,0 +1,18 @@
+package Strings;
+
+public class StringLiterals {
+
+	public static void main(String[] args) 
+	{
+		//StringObject//
+		String s1 =  new String("");
+		//StringLiteral//
+		String s2 = "Dhruv";
+		//output//
+		System.out.println(s1+ "_" +s2);
+		System.out.println(s1.length());
+		System.out.println(s1.isEmpty());
+		System.out.println(s1.isBlank());
+	}
+
+}
