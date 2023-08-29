34b80d5a3bd5be4922680391d00973c201f346fb
Elavarasu <elavarasu.subramaniam@gmail.com>
2023-08-29 16:04:18 +0530

    javaFile

---
+++ BinarySearch.java
@@ -0,0 +1,23 @@
+package Algorithms;
+
+public class BinarySearch {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+	  int nums [] = {-1,0,3,5,9,12};
+	  int target = 9;
+	  for(int i = 0; i<nums.length;i++)
+	  {
+		  if(nums[i] == target)
+		  {
+			  System.out.println(i);
+		  }
+		  else {
+			  return 
+		  }
+	  }
+
+	}
+
+}
